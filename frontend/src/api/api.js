f270d33d878b4c25fb06a64df264ecb2715bbb72
anuj931270 <133056513+anuj9162@users.noreply.github.com>
2026-09-03 15:24:18 +0530

    Fast_API_SQL

---
+++ frontend/src/api/api.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+const API_URL = "http://127.0.0.1:8000";
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const createStudent = async (studentData) => {
+  try {
+    const response = await api.post("/students", studentData);
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getStudents = async () => {
+  try {
+    const response = await api.get("/students");
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateStudent = async (studentId, studentData) => {
+  try {
+    const response = await api.put(`/students/${studentId}`, studentData);
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteStudent = async (studentId) => {
+  try {
+    const response = await api.delete(`/students/${studentId}`);
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default api;
